0e28a02d6ab9a980847d753bb36b81a10d4b0d39
Lukas Himsel <lukas@himsel.me>
2020-06-10 02:35:42 +0200

    rm form helper in annotations

--- firestore_annotations/lib/firestore_annotations.dart
+++ firestore_annotations/lib/firestore_annotations.dart
@@ -3,10 +3,8 @@ library firestore_annotations;
 
 class FirestoreDocument {
   final bool hasSelfRef;
-  final bool flutterFormHelper;
   const FirestoreDocument({
     this.hasSelfRef = true,
-    this.flutterFormHelper = false,
   });
 }
 
@@ -16,13 +14,11 @@ class FirestoreAttribute {
   final bool nullable;
   final String alias;
   final dynamic defaultValue;
-  final String flutterValidatorMessage;
 
   const FirestoreAttribute({
     this.ignore = false,
     this.nullable = true,
     this.alias = null,
     this.defaultValue = null,
-    this.flutterValidatorMessage = 'Couldn\'t parse input!',
   });
 }
